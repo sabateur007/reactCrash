67a17c21e3b00f40f9c48fa33e4f2be66c8dad3f
sabateur007 <aniketsingh0107@gmail.com>
2024-07-10 15:01:59 +0530

    custom react

--- basicreact/reactbasicsVite/src/main.jsx
+++ basicreact/reactbasicsVite/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+  return(
+    <div><h2>my app element hai ye rajau </h2></div>
+  )
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+      href:'https://www.youtube.com',
+      target:'_blank',
+  },
+  children: "click me to visit youtube"
+}
+
+const anotherelement =(
+  <a href="https://www.youtube.com" target='_blank'>visitt youthuub</a>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+anotherelement
 )
