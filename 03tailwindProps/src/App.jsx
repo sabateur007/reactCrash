35105e8f23e6243e49c16b08b50342d4e399a3ac
sabateur007 <aniketsingh0107@gmail.com>
2024-07-10 23:31:42 +0530

    card and props

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-green-400 rounded-xl p-2 text-white'>tailwind bolte</h1>
+    </>
+  )
+}
+
+export default App
