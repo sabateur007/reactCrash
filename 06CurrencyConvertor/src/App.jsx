b2fa5f7ae33d39f5856cd83433bed39853c5b8f9
sabateur007 <aniketsingh0107@gmail.com>
2024-07-16 20:35:11 +0530

    router push one

--- 06CurrencyConvertor/src/App.jsx
+++ 06CurrencyConvertor/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import useCurrencyInfo from './Hooks/usercurrencyinfo'
+import useCurrencyInfo from './Hooks/useCurrencyInfo'
 import './App.css'
-import {Input} from './components/Input'
-import useCurrencyInfo from './Hooks/usercurrencyinfo'
+import {Input} from './components/Index'
 
 function App() {
   const [amount, setAmount] = useState(0)
@@ -23,14 +22,63 @@ const swap=()=>
       return null
   }
 
-  
+  const convert = ()=>{
+    setConvertedAmount(amount * CurrencyInfo[to])
+  }
 
 
   return (
-    <>
-     <h1 className='bg-green-500 text-3xl text-blue text-center my-3'>currency convertor</h1>
-    </>
-  )
+    <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+            backgroundImage: `url('https://www.pexels.com/photo/silver-and-brown-round-coins-157520/')`,
+        }}
+    >
+        <div className="w-full">
+            <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                       convert();
+                    }}
+                >
+                    <div className="w-full mb-1">
+                        <Input
+                            label="From"
+                            amount = {amount}
+                            CurrencyOptions = {toCurrencyOptions}
+                            onCurrencyChange = {(currency)=> setAmount(amount)}
+                            selectcurrency = {from}
+                        />
+                    </div>
+                    <div className="relative w-full h-0.5">
+                        <button
+                            type="button"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            onClick={swap}
+                        >
+                            swap
+                        </button>
+                    </div>
+                    <div className="w-full mt-1 mb-4">
+                        <Input
+                            label="To"
+                            amount = {convertedAmoumt}
+                            CurrencyOptions = {toCurrencyOptions}
+                            onCurrencyChange = {(currency)=> setTo(currency)}
+                            selectcurrency = {to}
+                            amountdisable = {true}
+                            
+                        />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {from.toLowerCase()} to {to.toLowerCase()}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
 }
 
 export default App
--- 06CurrencyConvertor/src/Hooks/useCurrencyInfo.js
+++ 06CurrencyConvertor/src/Hooks/useCurrencyInfo.js
@@ -1,9 +1,10 @@
 import {useEffect, UseEffect, useState, UseState} from "react"
 
 function useCurrencyInfo(currency){
+    const [data,setdata] = useState({})
     useEffect(()=>{
         let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
-        const [data,setdata] = useState({})
+        
     fetch(url).then((res)=>res.json()).then((res)=>setdata(res[currency]))
     },[currency]);
     return data
