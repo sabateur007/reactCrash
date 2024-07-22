97d150cc6b1b30c6d6b8be39e64399034df87eb5
sabateur007 <aniketsingh0107@gmail.com>
2024-07-22 22:54:34 +0530

    redux toolkit, create featues reducers and store. next to do is components and using hooks to change state in store via redurcers.

---
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialstate = {
+    todos: [
+    {id: 1,
+    todotext : "this is a task to do"}
+    ]
+}
+
+export const todoSlice = createSlice({
+    name: todo,
+    initialState,
+    reducers:{
+         addTodo: (state, action)=>{
+            const todo = {
+                id : nanoid(),
+                todotext : action.payload
+            }   
+            state.todos.push(todo);
+         },
+         removeTodo: (state, action)=>{
+            state.todos = state.todos.filter(todo => todo.id!==action.payload)
+         }
+
+    }
+})
+
+export const {addTodo, removeTodo} = todoSlice.actions
+export default todoSlice.reducer
