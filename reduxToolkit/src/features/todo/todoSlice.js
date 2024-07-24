19d1179d8fb3bcaf4fb0331885ec43d2a8ad25c6
sabateur007 <aniketsingh0107@gmail.com>
2024-07-24 19:13:32 +0530

    update functionality completed

--- reduxToolkit/src/features/todo/todoSlice.js
+++ reduxToolkit/src/features/todo/todoSlice.js
@@ -21,10 +21,18 @@ export const todoSlice = createSlice({
          },
          removeTodo: (state, action)=>{
             state.todos = state.todos.filter(todo => todo.id!==action.payload)
+         },
+         updateTodo: (state, action)=>{
+            const { id, todotext } = action.payload;
+            const todo = state.todos.find((todo) => todo.id === id);
+            if (todo)
+             {
+                todo.todotext = todotext;
+             }
          }
 
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
 export default todoSlice.reducer
