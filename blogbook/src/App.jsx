4bfb8afee8b69ae0983fa8a0b6911cdd1f11d8a6
sabateur007 <aniketsingh0107@gmail.com>
2024-07-30 15:22:46 +0530

    header footer container cuurent user check

--- blogbook/src/App.jsx
+++ blogbook/src/App.jsx
@@ -1,13 +1,69 @@
-
+import {useDispatch} from 'react-redux'
+import { useState, useEffect } from 'react'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Header, Footer} from './components'
 import './App.css'
 
 function App() {
+const [loading, setLoading] =  useState(true);
+console.log(loading);
+const dispatch = useDispatch()
+// useEffect(()=>{
+//   const fetchCurrectUser = async ()=>{
+//     console.log("inside fetch current user")
+//     try{
+//       console.log("trying to run appwrite service")
+//       const currentUser = await authService.getCurrentUser();
+//       if(currentUser)
+//       {
+//         console.log("user found")
+//           dispatch(login(currentUser))
+//       }
+//       else{
+//           dispatch(logout())
+//           console.log("no user logged in")
+//       }
+//     }
+//     catch (error) {
+//         console.log("Appwrite serive :: getCurrentUser :: error", error);
+//     }
+//     setLoading(false);
+//   }
+  
+//   console.log(loading)
+// },[])
 
-  return (
+useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if (userData) {
+      dispatch(login({userData}))
+    } else {
+      dispatch(logout())
+    }
+  })
+  .finally(() => setLoading(false))
+}, [])
+
+
+  
+if(!loading){
+  return(
     <>
-      <h1>welcome to blogbook</h1>
+   <Header />
+   <div>main</div>
+   <Footer />
     </>
   )
+}
+else{
+  return(
+    <>
+    <h1>cant load the app</h1>
+    </>
+  )
+}
 }
 
 export default App
